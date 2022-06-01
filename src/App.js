2301b9439bd0e2996400ed6f9794f6e53045e4f7
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-06-01 11:19:54 +0200

    loginPage + google Auth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import CreatePost from './pages/CreatePost';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 
 function App() {
+    //state qui gère si le user est authentifié ou non:
+    //on passe le state aux autres componenets via les props setIsAuth
+    const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+    //pour se déconnecter, j'uilise la fct signOut de FB
+    const signUserOut = () => {
+        signOut(auth).then(() => {
+            //j'efface le localstorage:
+            localStorage.clear();
+            //je rdeconnecte le user en modifiant le state à false:
+            setIsAuth(false);
+            // je redirige vers la page se connecter:
+            //ici je ne peux pas utiliser useNavigate car je suis à l'extR du <Router></Router>
+            window.location.pathname = "/login";
+        });
+    };
+
     return (
         <Router>
             <nav>
                 <Link to="/"> Home </Link>
+                {/* si je ne suis pas connecté, je vois le lien pour se logger: 
+et à l'inverse quand je suis connecté, je ne vois plus le lien Se connecter */}
+                {!isAuth ? (
+                    <Link to="/login"> Login </Link>
+                ) : (
+                    <>
+                        {/* et si je suis connecté, montrer le bouton Se déconnecter: */}
+                        <Link to="/createpost"> Create Post </Link>
+                        <button onClick={signUserOut}> Log Out</button>
+                    </>
+                )}
             </nav>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/createpost" element={<CreatePost />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Home isAuth={isAuth} />} />
+                <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
+                <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
             </Routes>
         </Router>);
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
+//je récupère le state setIsAUth via les props
+function Login({ setIsAuth }) {
+    let navigate = useNavigate();
+
+    //se connecter via Goggle:
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider).then((result) => {
+            //je stocke la paire clé/valeur dans le localstorage:   setItem(nomClé, valeurClé) => Inspecteur - Appli - LocalStorage
+            localStorage.setItem("isAuth", true);
+            //quand on est connecté, on passe le setIsAuth à TRUE:
+            setIsAuth(true);
+            //après connexion on est redirigé sur la page Accueil /
+            navigate("/");
+        });
+    };
+
     return (
-        <div>Login</div>
-    )
+        <div className="loginPage">
+            <p>Sign In With Google to Continue</p>
+            <button className="login-with-google-btn" onClick={signInWithGoogle}>
+                Sign in with Google
+            </button>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
