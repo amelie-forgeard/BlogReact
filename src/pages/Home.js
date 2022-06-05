c9d05a941828795903914073cbd3c8091d4f33ff
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-06-05 11:44:28 +0200

    deletePost fct is working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,9 @@ import { db } from "../firebase-config";
 function Home() {
     const [postLists, setPostList] = useState([]);
 
+    // deleteDoc fonction de firestore pour supprimer:
+    //doc de firestore désigne quelle donnée on veut viser
+    // id renvoit au post sur lequel on a cliqué sur la corbeille delete
     const deletePost = async (id) => {
         const postDoc = doc(db, "posts", id)
         await deleteDoc(postDoc);
@@ -13,6 +16,12 @@ function Home() {
 
     // variable qui renvoit à la table bdd de firebase avec "posts" le nom de la table que nous avons crée dans le Firestore
     const postsCollectionRef = collection(db, "posts");
+    //on utilise le useEffect pour intégrer les articles de la bdd Firebase => dans le rendu de ma page:
+    // je déclare:  fonction getPost qui récupère les articles de la bdd via getDocs de Firebase qui récupère tous les données de la table posts
+    // je fais un map sur les datas que je récupère, et pour chaque doc je déverse toutes les datas avec le spread opartor et je crée un Id
+    // cela permet de récupérer un simple array avec author + id + postText + title
+    // set PostList fonction de modification qui va afficher ce que j'ai récupé avec le map:
+    // console.log((data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))));
     useEffect(() => {
         const getPosts = async () => {
             const data = await getDocs(postsCollectionRef);
@@ -22,27 +31,6 @@ function Home() {
         getPosts();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [deletePost]);
-    //on utilise le useEffect pour intégrer les articles de la bdd Firebase => dans le rendu de ma page:
-    // useEffect(() => {
-    //     // je déclare:  fonction getPost qui récupère les articles de la bdd via getDocs de Firebase qui récupère tous les données de la table posts
-
-    //     const getPosts = async () => {
-    //         const data = await getDocs(postsCollectionRef);
-    //         // je fais un map sur les datas que je récupère, et pour chaque doc je déverse toutes les datas avec le spread opartor et je crée un Id
-    //         // cela permet de récupérer un simple array avec author + id + postText + title
-    //         // set PostList fonction de modification qui va afficher ce que j'ai récupé avec le map:
-
-    //         // console.log((data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))));
-    //         setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    //     };
-    //     // j'appelle la fonction dans le useEffect:
-
-    //     getPosts();
-    // }, [deletePost]);
-
-    // deleteDoc fonction de firestore pour supprimer:
-    //doc de firestore désigne quelle donnée on veut viser
-    // id renvoit au post sur lequel on a cliqué sur la corbeille delete
     return (
         <div className="homePage">
             {
@@ -62,7 +50,8 @@ function Home() {
                                 >
                                     {" "}
                                     &#128465;
-                                </button>                            </div>
+                                </button>
+                            </div>
                         </div>
                         <div className="postTextContainer">
                             {post.postText}
